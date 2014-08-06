576a0aa1fc800009c3888f78c8048456703606e4
seogi1004 <alvin@jennifersoft.com>
2014-08-06 10:57:09 +0900

    refactoring PathElement class

--- js/util/svg.js
+++ js/util/svg.js
@@ -181,94 +181,113 @@ jui.define("util.svg.element.transform", [], function() { // polygon, polyline
 
 jui.define("util.svg.element.path", [], function() { // path
     var PathElement = function() {
-        var orders = [],
-            isUpper = true;
+        var orders = [];
 
         function applyOrders(self) {
             self.attr({ d: orders.join(" ") });
         }
 
-        this.uppercase = function() {
-            isUpper = true;
-            return this;
-        }
-
-        this.lowercase = function() {
-            isUpper = false;
-            return this;
-        }
-
-        this.moveTo = function(x, y) {
-            orders.push( ((isUpper) ? "M" : "m") + x + "," + y );
+        this.moveTo = function(x, y, type) {
+            orders.push( (type || "m") + x + "," + y );
             applyOrders(this);
 
             return this;
         }
+        this.MoveTo = function(x, y) {
+            return this.moveTo(x, y, "M");
+        }
 
-        this.lineTo = function(x, y) {
-            orders.push( ((isUpper) ? "L" : "l") + x + "," + y );
+        this.lineTo = function(x, y, type) {
+            orders.push( (type || "l") + x + "," + y );
             applyOrders(this);
 
             return this;
         }
+        this.LineTo = function(x, y) {
+            return this.lineTo(x, y, "L");
+        }
 
-        this.hLineTo = function(x) {
-            orders.push( ((isUpper) ? "H" : "h") + x );
+        this.hLineTo = function(x, type) {
+            orders.push( (type || "h") + x );
             applyOrders(this);
 
             return this;
         }
+        this.HLineTo = function(x) {
+            return this.hLineTo(x, "H");
+        }
 
-        this.vLineTo = function(y) {
-            orders.push( ((isUpper) ? "V" : "v") + y );
+        this.vLineTo = function(y, type) {
+            orders.push( (type || "v") + y );
             applyOrders(this);
 
             return this;
         }
+        this.VLineTo = function(y) {
+            return this.vLineTo(y, "V");
+        }
 
-        this.curveTo = function(x1, y1, x2, y2, x, y) {
-            orders.push( ((isUpper) ? "C" : "c") + x1 + "," + y1 + " " + x2 + "," + y2 + " " + x + "," + y );
+        this.curveTo = function(x1, y1, x2, y2, x, y, type) {
+            orders.push( (type || "c") + x1 + "," + y1 + " " + x2 + "," + y2 + " " + x + "," + y );
             applyOrders(this);
 
             return this;
         }
+        this.CurveTo = function(x1, y1, x2, y2, x, y) {
+            return this.curveTo(x1, y1, x2, y2, x, y, "C");
+        }
 
-        this.sCurveTo = function(x2, y2, x, y) {
-            orders.push( ((isUpper) ? "S" : "s") + x2 + "," + y2 + " " + x + "," + y );
+        this.sCurveTo = function(x2, y2, x, y, type) {
+            orders.push( (type || "s") + x2 + "," + y2 + " " + x + "," + y );
             applyOrders(this);
 
             return this;
         }
+        this.SCurveTo = function(x2, y2, x, y) {
+            return this.sCurveTo(x2, y2, x, y, "S");
+        }
 
-        this.qCurveTo = function(x1, y1, x, y) {
-            orders.push( ((isUpper) ? "Q" : "q") + x1 + "," + y1 + " " + x + "," + y );
+        this.qCurveTo = function(x1, y1, x, y, type) {
+            orders.push( (type || "q") + x1 + "," + y1 + " " + x + "," + y );
             applyOrders(this);
 
             return this;
         }
+        this.QCurveTo = function(x1, y1, x, y) {
+            return this.qCurveTo(x1, y1, x, y, "Q");
+        }
 
-        this.tCurveTo = function(x1, y1, x, y) {
-            orders.push( ((isUpper) ? "T" : "t") + x1 + "," + y1 + " " + x + "," + y );
+        this.tCurveTo = function(x1, y1, x, y, type) {
+            orders.push( (type || "t") + x1 + "," + y1 + " " + x + "," + y );
             applyOrders(this);
 
             return this;
         }
+        this.TCurveTo = function(x1, y1, x, y) {
+            return this.tCurveTo(x1, y1, x, y, "T");
+        }
 
-        this.arc = function(rx, ry, x_axis_rotation, large_arc_flag, sweep_flag, x, y) {
+        this.arc = function(rx, ry, x_axis_rotation, large_arc_flag, sweep_flag, x, y, type) {
             large_arc_flag = (large_arc_flag) ? 1 : 0;
             sweep_flag = (sweep_flag) ? 1 : 0;
 
-            orders.push( ((isUpper) ? "A" : "a") + rx + "," + ry + " " + x_axis_rotation + " " + large_arc_flag + "," + sweep_flag + " " + x + "," + y );
+            orders.push( (type || "a") + rx + "," + ry + " " + x_axis_rotation + " " + large_arc_flag + "," + sweep_flag + " " + x + "," + y );
             applyOrders(this);
 
             return this;
         }
+        this.Arc = function(rx, ry, x_axis_rotation, large_arc_flag, sweep_flag, x, y) {
+            return this.arc(rx, ry, x_axis_rotation, large_arc_flag, sweep_flag, x, y, "A");
+        }
 
-        this.closePath = function() {
-            orders.push((isUpper) ? "Z" : "z");
+        this.closePath = function(type) {
+            orders.push( (type || "z") );
             applyOrders(this);
 
             return this;
+        }
+        this.ClosePath = function() {
+            return this.closePath("Z");
         }
     }
 
