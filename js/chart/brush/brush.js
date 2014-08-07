ee574cae914abeabd2f2d95947ee889ae8e883fd
seogi1004 <alvin@jennifersoft.com>
2014-08-08 01:21:23 +0900

    modify line graph...

--- js/chart/brush/brush.js
+++ js/chart/brush/brush.js
@@ -35,6 +35,59 @@ jui.define("chart.brush", [], function() {
 
             return "rgb(" + colors[colorIndex++] +")";
         }
+
+        this.curvePoints = function(K) {
+            var p1 = [];
+            var p2 = [];
+            var n = K.length - 1;
+
+            /*rhs vector*/
+            var a = [];
+            var b = [];
+            var c = [];
+            var r = [];
+
+            /*left most segment*/
+            a[0] = 0;
+            b[0] = 2;
+            c[0] = 1;
+            r[0] = K[0] + 2 * K[1];
+
+            /*internal segments*/
+            for (i = 1; i < n - 1; i++)
+            {
+                a[i] = 1;
+                b[i] = 4;
+                c[i] = 1;
+                r[i] = 4 * K[i] + 2 * K[i + 1];
+            }
+
+            /*right segment*/
+            a[n-1] = 2;
+            b[n-1] = 7;
+            c[n-1] = 0;
+            r[n-1] = 8 * K[n-1] + K[n];
+
+            /*solves Ax=b with the Thomas algorithm (from Wikipedia)*/
+            for (var i = 1; i < n; i++)
+            {
+                var m = a[i] / b[i - 1];
+                b[i] = b[i] - m * c[i - 1];
+                r[i] = r[i] - m * r[i - 1];
+            }
+
+            p1[n - 1] = r[n - 1] / b[n - 1];
+            for (var i = n - 2; i >= 0; --i)
+                p1[i] = (r[i] - c[i] * p1[i+1]) / b[i];
+
+            /*we have p1, now compute p2*/
+            for (var i = 0; i < n - 1; i++)
+                p2[i] = 2 * K[i + 1] - p1[i + 1];
+
+            p2[n - 1] = 0.5 * (K[n] + p1[n-1]);
+
+            return { p1: p1, p2: p2 };
+        }
     }
 
     return Brush;
--- js/chart/brush/line.js
+++ js/chart/brush/line.js
@@ -3,59 +3,6 @@ jui.define("chart.brush.line", [], function() {
     var LineBrush = function(grid) {
         var g, zeroY, series, count, width;
 
-        function computeControlPoints(K) {
-            var p1 = [];
-            var p2 = [];
-            var n = K.length - 1;
-
-            /*rhs vector*/
-            var a = [];
-            var b = [];
-            var c = [];
-            var r = [];
-
-            /*left most segment*/
-            a[0] = 0;
-            b[0] = 2;
-            c[0] = 1;
-            r[0] = K[0]+2*K[1];
-
-            /*internal segments*/
-            for (i = 1; i < n - 1; i++)
-            {
-                a[i] = 1;
-                b[i] = 4;
-                c[i] = 1;
-                r[i] = 4 * K[i] + 2 * K[i + 1];
-            }
-
-            /*right segment*/
-            a[n-1] = 2;
-            b[n-1] = 7;
-            c[n-1] = 0;
-            r[n-1] = 8 * K[n-1] + K[n];
-
-            /*solves Ax=b with the Thomas algorithm (from Wikipedia)*/
-            for (var i = 1; i < n; i++)
-            {
-                var m = a[i] / b[i - 1];
-                b[i] = b[i] - m * c[i - 1];
-                r[i] = r[i] - m * r[i - 1];
-            }
-
-            p1[n - 1] = r[n - 1] / b[n - 1];
-            for (var i = n - 2; i >= 0; --i)
-                p1[i] = (r[i] - c[i] * p1[i+1]) / b[i];
-
-            /*we have p1, now compute p2*/
-            for (var i = 0; i < n - 1; i++)
-                p2[i] = 2 * K[i + 1] - p1[i + 1];
-
-            p2[n - 1] = 0.5 * (K[n] + p1[n-1]);
-
-            return { p1: p1, p2: p2 };
-        }
-
         this.drawBefore = function(chart) {
             g = chart.svg.group().translate(chart.area.x, chart.area.y);
 
@@ -92,8 +39,8 @@ jui.define("chart.brush.line", [], function() {
 
                 var x = path[k].x,
                     y = path[k].y,
-                    px = computeControlPoints(x),
-                    py = computeControlPoints(y);
+                    px = this.curvePoints(x),
+                    py = this.curvePoints(y);
 
                 for(var i = 0; i < x.length - 1; i++) {
                     p.MoveTo(x[i], y[i]);
