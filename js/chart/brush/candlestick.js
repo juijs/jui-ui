6c0bc76af78f6c7bc275ee4c5316b348dae20cc4
seogi1004 <alvin@jennifersoft.com>
2014-08-24 20:55:22 +0900

    test

---
+++ js/chart/brush/candlestick.js
@@ -0,0 +1,28 @@
+jui.define("chart.brush.candlestick", [], function() {
+
+	var CandleStickBrush = function(brush) {
+        var g, zeroY, count, width, barWidth;
+
+        this.drawBefore = function(chart) {
+            g = chart.svg.group().translate(chart.area('x'), chart.area('y'));
+
+            zeroY = brush.y(0);
+            count = chart.data().length;
+
+            //width = brush.x(1) + brush.x(0);
+            barWidth = width * 0.7;
+
+            console.log(width);
+        }
+
+        this.draw = function(chart) {
+            /*/
+            for (var i = 0; i < count; i++) {
+                console.log(brush.x(i));
+            }
+            /**/
+        }
+	}
+
+	return CandleStickBrush;
+}, "chart.brush");
