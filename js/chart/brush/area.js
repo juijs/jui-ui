9816612e84cc7312dc12f10154351b3ea956ec0b
seogi1004 <alvin@jennifersoft.com>
2014-08-29 19:47:36 +0900

    refactoring 'area' brush  - set parent module (line)

--- js/chart/brush/area.js
+++ js/chart/brush/area.js
@@ -1,47 +1,24 @@
 jui.define("chart.brush.area", [], function() {
 
     var AreaBrush = function(brush) {
-        var self = this;
-
-        function createArea(brush, chart, pos, index) {
-            var x = pos.x,
-                y = pos.y,
-                maxY = chart.height();
-
-            var p = chart.svg.path({
-                fill : chart.theme.color(index),
-                opacity : chart.theme("areaOpacity")
-            }).MoveTo(x[0], y[0]);
-
-            if(brush.symbol == "curve") {
-                var px = self.curvePoints(x),
-                    py = self.curvePoints(y);
-
-                for (var i = 0; i < x.length - 1; i++) {
-                    p.CurveTo(px.p1[i], py.p1[i], px.p2[i], py.p2[i], x[i + 1], y[i + 1]);
-                }
-            } else {
-                for (var i = 0; i < x.length - 1; i++) {
-                    if(brush.symbol == "step") {
-                        p.LineTo(x[i], y[i + 1]);
-                    }
-
-                    p.LineTo(x[i + 1], y[i + 1]);
-                }
-            }
-
-            p.LineTo(x[x.length - 1], maxY);
-            p.LineTo(x[0], maxY);
-            p.ClosePath();
-
-            return p;
-        }
 
         this.drawArea = function(brush, chart, path) {
-            var g = chart.svg.group().translate(chart.x(), chart.y());
+            var g = chart.svg.group().translate(chart.x(), chart.y()),
+                maxY = chart.height();
 
             for (var k = 0; k < path.length; k++) {
-                var p = createArea(brush, chart, path[k], k);
+                var p = this.createLine(brush, chart, path[k], k),
+                    xList = path[k].x;
+
+                p.LineTo(xList[xList.length - 1], maxY);
+                p.LineTo(xList[0], maxY);
+                p.ClosePath();
+                p.attr({
+                    fill: chart.theme.color(k),
+                    "fill-opacity": chart.theme("areaOpacity"),
+                    "stroke-width": 0
+                });
+
                 g.prepend(p);
             }
         }
@@ -52,4 +29,4 @@ jui.define("chart.brush.area", [], function() {
     }
 
     return AreaBrush;
-}, "chart.brush.core");
+}, "chart.brush.line");
