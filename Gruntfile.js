ec2494196f48b66158803b44602768d5fa4a7747
seogi1004 <alvin@jennifersoft.com>
2014-10-01 11:18:03 +0900

    grunt option changed!!!

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,129 +1,126 @@
 module.exports = function(grunt) {
-
-  grunt.initConfig({
-    qunit: {
-        options: {
-            timeout: 10000
+    grunt.initConfig({
+        qunit: {
+            options: {
+                timeout: 10000
+            },
+            all: [ "test/*.html", "test/*/*.html" ]
         },
-        all: [ 'test/*.html', 'test/*/*.html' ]
-    },
-    concat : {
-        dist : {
-            src : [
-                // core
-                "js/base.js",
-                "js/core.js",
+        concat : {
+            dist : {
+                src : [
+                    // core
+                    "js/base.js",
+                    "js/core.js",
 
-                // util
-                "js/util/math.js",
-                "js/util/time.js",
-                "js/util/scale.js",
-                "js/util/svg.js",
+                    // util
+                    "js/util/math.js",
+                    "js/util/time.js",
+                    "js/util/scale.js",
+                    "js/util/svg.js",
 
-                // ui
-                "js/ui/button.js",
-                "js/ui/combo.js",
-                "js/ui/datepicker.js",
-                "js/ui/dropdown.js",
-                "js/ui/modal.js",
-                "js/ui/notify.js",
-                "js/ui/paging.js",
-                "js/ui/tooltip.js",
-                "js/ui/layout.js",
+                    // ui
+                    "js/ui/button.js",
+                    "js/ui/combo.js",
+                    "js/ui/datepicker.js",
+                    "js/ui/dropdown.js",
+                    "js/ui/modal.js",
+                    "js/ui/notify.js",
+                    "js/ui/paging.js",
+                    "js/ui/tooltip.js",
+                    "js/ui/layout.js",
 
-                // uix
-                "js/uix/autocomplete.js",
-                "js/uix/tab.js",
-                "js/uix/table.js",
-                "js/uix/tree.js",
-                "js/uix/window.js",
-                "js/uix/xtable.js",
+                    // uix
+                    "js/uix/autocomplete.js",
+                    "js/uix/tab.js",
+                    "js/uix/table.js",
+                    "js/uix/tree.js",
+                    "js/uix/window.js",
+                    "js/uix/xtable.js",
 
-                // chart (core)
-                "js/chart/draw.js",
-                "js/chart/builder.js",
+                    // chart (core)
+                    "js/chart/draw.js",
+                    "js/chart/builder.js",
 
-                // chart.theme
-                "js/chart/theme/jennifer.js",
-                "js/chart/theme/dark.js",                
-                "js/chart/theme/seoul.js",
+                    // chart.theme
+                    "js/chart/theme/jennifer.js",
+                    "js/chart/theme/dark.js",
+                    "js/chart/theme/seoul.js",
 
-                // chart.grid
-                "js/chart/grid/core.js",
-                "js/chart/grid/block.js",
-                "js/chart/grid/date.js",
-                "js/chart/grid/radar.js",
-                "js/chart/grid/range.js",
+                    // chart.grid
+                    "js/chart/grid/core.js",
+                    "js/chart/grid/block.js",
+                    "js/chart/grid/date.js",
+                    "js/chart/grid/radar.js",
+                    "js/chart/grid/range.js",
 
-                // chart.brush
-                "js/chart/brush/core.js",
-                "js/chart/brush/bar.js",
-                "js/chart/brush/bubble.js",
-                "js/chart/brush/candlestick.js",
-                "js/chart/brush/ohlc.js",
-                "js/chart/brush/column.js",
-                "js/chart/brush/donut.js",
-                "js/chart/brush/equalizer.js",
-                "js/chart/brush/fullstack.js",
-                "js/chart/brush/line.js",
-                "js/chart/brush/path.js",
-                "js/chart/brush/pie.js",
-                "js/chart/brush/scatter.js",
-                "js/chart/brush/stackbar.js",
-                "js/chart/brush/stackcolumn.js",
-                "js/chart/brush/bargauge.js",
-                "js/chart/brush/circlegauge.js",
-                "js/chart/brush/fillgauge.js",
-                "js/chart/brush/area.js", // extends line
-                "js/chart/brush/stackline.js", // extends line
-                "js/chart/brush/stackarea.js", // extends area
-                "js/chart/brush/stackscatter.js", // extends scatter
-                "js/chart/brush/gauge.js", // extends donut
-                "js/chart/brush/fullgauge.js", // extends donut
-                "js/chart/brush/stackgauge.js", // extends donut
+                    // chart.brush
+                    "js/chart/brush/core.js",
+                    "js/chart/brush/bar.js",
+                    "js/chart/brush/bubble.js",
+                    "js/chart/brush/candlestick.js",
+                    "js/chart/brush/ohlc.js",
+                    "js/chart/brush/column.js",
+                    "js/chart/brush/donut.js",
+                    "js/chart/brush/equalizer.js",
+                    "js/chart/brush/fullstack.js",
+                    "js/chart/brush/line.js",
+                    "js/chart/brush/path.js",
+                    "js/chart/brush/pie.js",
+                    "js/chart/brush/scatter.js",
+                    "js/chart/brush/stackbar.js",
+                    "js/chart/brush/stackcolumn.js",
+                    "js/chart/brush/bargauge.js",
+                    "js/chart/brush/circlegauge.js",
+                    "js/chart/brush/fillgauge.js",
+                    "js/chart/brush/area.js", // extends line
+                    "js/chart/brush/stackline.js", // extends line
+                    "js/chart/brush/stackarea.js", // extends area
+                    "js/chart/brush/stackscatter.js", // extends scatter
+                    "js/chart/brush/gauge.js", // extends donut
+                    "js/chart/brush/fullgauge.js", // extends donut
+                    "js/chart/brush/stackgauge.js", // extends donut
 
-                // chart.widget
-                "js/chart/widget/tooltip.js",
-                "js/chart/widget/title.js",
-                "js/chart/widget/legend.js"
-            ],
-            dest : "jui.js"
-        }
-    },
+                    // chart.widget
+                    "js/chart/widget/tooltip.js",
+                    "js/chart/widget/title.js",
+                    "js/chart/widget/legend.js"
+                ],
+                dest : "jui.js"
+            }
+        },
 
-    uglify: {
+        uglify: {
+            dist : {
+                files : {
+                    "jui.min.js" : [ "jui.js" ]
+                }
+            }
+        },
+        cssmin: {
+            dist: {
+                files: {
+                    "jui.min.css": "jui.css"
+                }
+            }
+        },
 
-      dist : {
-        files : {
-            "jui.min.js" : [ "jui.js" ]
-        }
-      }
-    },
-    cssmin: {
-      dist: {
-        files: {
-          'jui.min.css': 'jui.css'
-        }
-      }
-    },
+        less: {
+            dist: {
+                files: {
+                    "jui.css" : [
+                        "less/_main.less"
+                    ]
+                }
+            }
+        },
+        pkg: grunt.file.readJSON("package.json")
+    });
 
-    less: {
-      dist: {
-        files: {
-            "jui.css" : [
-                "less/_main.less"
-            ]
-        }
-      }
-    },
-    pkg: grunt.file.readJSON('package.json')
-  });
+    require("load-grunt-tasks")(grunt);
 
-  require('load-grunt-tasks')(grunt);
-
-  grunt.registerTask('default', ["concat", 'uglify' ]);
-  
-  grunt.registerTask('all', [ 'less', 'cssmin', "concat", 'uglify' ]);
-  
-  grunt.registerTask('test', [ "qunit" ]);
+    grunt.registerTask("default", [ "concat", "uglify" ]);
+    grunt.registerTask("css", [ "less", "cssmin" ]);
+    grunt.registerTask("test", [ "qunit" ]);
+    grunt.registerTask("all", [ "css", "test", "default" ]);
 };
