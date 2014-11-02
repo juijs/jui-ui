d0b0301c3a91bc0cc245a32dfe24d456fb640f86
seogi1004 <alvin@jennifersoft.com>
2014-11-03 00:40:05 +0900

    zoom close button bugfixed in chart

--- jui.js
+++ jui.js
@@ -15851,9 +15851,7 @@ jui.define("chart.widget.zoom", [ "util.base" ], function(_) {
 
                         chart.svg.path({
                             d: "M12,2C6.5,2,2,6.5,2,12c0,5.5,4.5,10,10,10s10-4.5,10-10C22,6.5,17.5,2,12,2z M16.9,15.5l-1.4,1.4L12,13.4l-3.5,3.5   l-1.4-1.4l3.5-3.5L7.1,8.5l1.4-1.4l3.5,3.5l3.5-3.5l1.4,1.4L13.4,12L16.9,15.5z",
-                            fill: chart.theme("zoomFocusColor"),
-                            x: cw - r,
-                            y: -r
+                            fill: chart.theme("zoomFocusColor")
                         }).translate(cw - r, -r);
                     }).on("click", function(e) {
                         bg.attr({ visibility: "hidden" });
