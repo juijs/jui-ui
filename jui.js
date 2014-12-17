3c7f3a6a519cbd1c23a0cfbbc6747016004d2c77
seogi1004 <alvin@jennifersoft.com>
2014-12-17 17:57:14 +0900

    code refactoring...

--- jui.js
+++ jui.js
@@ -16711,7 +16711,7 @@ jui.define("chart.widget.tooltip", [ "jquery" ], function($) {
 
     return TooltipWidget;
 }, "chart.widget.core");
-jui.define("chart.widget.title", [ "util.base", "util.math" ], function(_, math) {
+jui.define("chart.widget.title", [], function() {
 
     var TitleWidget = function(chart, widget) {
         var x = 0, y = 0, anchor = "middle";
@@ -16738,14 +16738,10 @@ jui.define("chart.widget.title", [ "util.base", "util.math" ], function(_, math)
         }
 
         this.draw = function() {
-            if (widget.text == "") {
-                return; 
-            }
-
             var obj = chart.svg.getTextRect(widget.text);
 
-            var half_text_width = obj.width/2;
-            var half_text_height = obj.height/2;
+            var half_text_width = obj.width / 2,
+                half_text_height = obj.height / 2;
 
             var text =  chart.text({
                 x : x + widget.dx,
@@ -16762,7 +16758,6 @@ jui.define("chart.widget.title", [ "util.base", "util.math" ], function(_, math)
                 } else if (widget.align == 'end') {
                     text.rotate(90, x + widget.dx - half_text_width, y + widget.dy + half_text_height)
                 }
-
             }
 
             return text;
