aa62eca55e0950710ffe36210abf70300292e7ce
easylogic <cyberuls@gmail.com>
2015-01-17 03:22:26 +0900

    add jsduck

--- js/chart/builder.js
+++ js/chart/builder.js
@@ -873,19 +873,19 @@ jui.defineUI("chart.builder", [ "jquery", "util.base", "util.svg", "util.color",
     }
 
     /**
-     * @event bg\.click
+     * @event bg/click
      * Fired when chart bg is clicked on.
      * @param {jQueryEvent} e The event object.
      */
 
     /**
-     * @event chart.click
+     * @event chart:click
      * Fired when chart is clicked on.
      * @param {jQueryEvent} e The event object.
      */
 
     /**
-     * @event "chart.rclick"
+     * @event "chart#rclick"
      * Fired when chart is right clicked on.
      * @param {jQueryEvent} e The event object.
      */
