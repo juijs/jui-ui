b67e62e1d12b4a5a16b401dde55a2797e44375d5
jinho park <cyberuls@gmail.com>
2015-01-20 16:45:51 +0900

    refactoring grid code

--- js/chartx/realtime.js
+++ js/chartx/realtime.js
@@ -1,5 +1,12 @@
 jui.defineUI("chartx.realtime", [ "jquery", "util.base", "util.time", "chart.builder" ], function($, _, time, builder) {
 
+    /**
+     * @class chartx.realtime
+     *
+     * 리얼타임 차트 구현
+     *
+     * @extends core
+     */
     var UI = function() {
         var axis = null,
             interval = null,
@@ -140,25 +147,35 @@ jui.defineUI("chartx.realtime", [ "jquery", "util.base", "util.time", "chart.bui
 
     UI.setup = function() {
         return {
-            width : "100%",		// chart 기본 넓이
-            height : "100%",		// chart 기본 높이
+            /** @cfg {String/Number} [width="100%"] 차트 기본 넓이 */
+            width : "100%",
+            /** @cfg {String/Number} [height="100%"] 차트 기본 높이 */
+            height : "100%",
 
-            // style
+            /** @cfg {Object} padding 차트 여백 */
             padding : {
+                /** @cfg {Number} [padding.left=50] */
                 left : 50 ,
+                /** @cfg {Number} [padding.right=50] */
                 right : 50,
+                /** @cfg {Number} [padding.bottom=50] */
                 bottom : 50,
+                /** @cfg {Number} [padding.top=50] */
                 top : 50
             },
 
-            // chart
-            theme : "jennifer",	// 기본 테마 jennifer
+            /** @cfg {String} [theme=jennifer] 기본 테마 jennifer */
+            theme : "jennifer",
+            /** @cfg {Object} [style={}]  */
             style : {},
+            /** @cfg {Object} [series={}] */
             series : {},
+            /** @cfg {Array} [brush=[]]  */
             brush : [],
+            /** @cfg {Array} [widget=[]] */
             widget : [],
 
-            // grid (custom)
+            /** @cfg {Object} axis  그리드 에 관한 설정 */
             axis : {
                 domain : null,
                 format : "hh:mm",
@@ -170,8 +187,10 @@ jui.defineUI("chartx.realtime", [ "jquery", "util.base", "util.time", "chart.bui
                 data : []
             },
 
-            // realtime
+            /** @cfg {Number} [interval=1] 리얼타임 움직이는 시간 간격(초단위) */
             interval : 1, // 초
+
+            /** @cfg {Number} [interval=1] 리얼타임 전체 시작과 끝 기간 (분단위) */
             period : 5 // 분
         }
     }
