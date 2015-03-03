accacc1643a5e7ab1574778b575cb72ce542ed80
jinho park <cyberuls@gmail.com>
2015-03-03 17:58:50 +0900

    fixed nodejs support

--- jui.node.js
+++ jui.node.js
@@ -3,5 +3,6 @@ global.document = jsdom.jsdom();
 global.window = document.parentWindow;
 global.jQuery = global.$ = require('jquery');
 
-var jui = require('./jui').jui;
-module.exports = jui;
+require('./jui');
+
+module.exports = global.jui;
