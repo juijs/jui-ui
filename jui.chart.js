a18c47b2d6b213701914896982ee63290afe93a8
seogi1004 <seogi777@gmail.com>
2015-06-25 01:21:15 +0900

    Map Selector 브러쉬 개발 중...  - 툴팁

--- jui.chart.js
+++ jui.chart.js
@@ -17739,6 +17739,9 @@ jui.define("chart.brush.map.core", [ "jquery", "util.base" ], function($, _) {
 	return MapCoreBrush;
 }, "chart.brush.core");
 jui.define("chart.brush.map.selector", [ "util.base" ], function(_) {
+	var PADDING = 7,
+		ANCHOR = 7,
+		TEXT_Y = 14;
 
     /**
      * @class chart.brush.over 
@@ -17746,11 +17749,56 @@ jui.define("chart.brush.map.selector", [ "util.base" ], function(_) {
      * @extends chart.brush.core
      */
 	var MapSelectorBrush = function(chart, axis, brush) {
-		var activePath = null;
+		var self = this;
+		var g = null, activePath = null;
+
+		this.drawTooltip = function() {
+			this.eachData(function(i, d) {
+				var id = axis.getValue(d, "id", null),
+					value = axis.getValue(d, "value", 0),
+					text = id + ": " + value,
+					xy = axis.map(id);
+
+				if(_.typeCheck("function", brush.format)) {
+					text = self.format(d);
+				}
+
+				var size = chart.svg.getTextSize(text),
+					w = size.width + (PADDING * 2),
+					h = size.height;
+
+				if(xy != null) {
+					var tooltip = chart.svg.group({
+						visibility: "hidden"
+					}, function() {
+						chart.svg.polygon({
+							points: self.balloonPoints("top", w, h, ANCHOR),
+							fill: chart.theme("tooltipBackgroundColor"),
+							"fill-opacity": chart.theme("tooltipBackgroundOpacity"),
+							stroke: chart.theme("tooltipBorderColor"),
+							"stroke-width": 1
+						});
+
+						chart.text({
+							"font-size": chart.theme("tooltipFontSize"),
+							"fill": chart.theme("tooltipFontColor"),
+							"text-anchor": "middle",
+							x: w / 2,
+							y: TEXT_Y
+						}).html(text);
+					}).translate(xy.x - (w / 2), xy.y - h - ANCHOR);
+
+					g.append(tooltip);
+				}
+			});
+		}
+
+		this.drawBefore = function() {
+			g = chart.svg.group();
+		}
 
 		this.draw = function() {
-			var g = chart.svg.group(),
-				originFill = null;
+			var originFill = null;
 
 			this.on("map.mouseover", function(obj, e) {
 				if(activePath == obj.path) return;
@@ -17784,6 +17832,8 @@ jui.define("chart.brush.map.selector", [ "util.base" ], function(_) {
 				});
 			}
 
+			this.drawTooltip();
+
 			return g;
 		}
 	}
@@ -17791,7 +17841,8 @@ jui.define("chart.brush.map.selector", [ "util.base" ], function(_) {
 	MapSelectorBrush.setup = function() {
 		return {
 			activeEvent: null,
-			active: null
+			active: null,
+			format: null
 		}
 	}
 
