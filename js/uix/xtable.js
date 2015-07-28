124bb91144b9ffca850314fdf60ad893b4d95ac0
seogi1004 <seogi777@gmail.com>
2015-07-29 01:03:42 +0900

    X-Table API 추가

--- js/uix/xtable.js
+++ js/uix/xtable.js
@@ -1,10 +1,6 @@
 jui.defineUI("uix.xtable", [ "jquery", "util.base", "ui.modal", "uix.table" ], function($, _, modal, table) {
 	var p_type = null;
 
-	/**
-	 * Common Logic
-	 * 
-	 */
 	_.resize(function() {
 		var call_list = jui.get("uix.xtable");
 		
@@ -19,9 +15,8 @@ jui.defineUI("uix.xtable", [ "jquery", "util.base", "ui.modal", "uix.table" ], f
 
     /**
      * @class uix.xtable
-     * implements XTable for Large Data
      * @extends core
-     * @alias XTable
+     * @alias X-Table
      * @requires util.base
      * @requires ui.modal
      * @requires uix.table
@@ -33,11 +28,7 @@ jui.defineUI("uix.xtable", [ "jquery", "util.base", "ui.modal", "uix.table" ], f
 		var ui_modal = null, page = 1;
         var is_loading = false, is_resize = false;
 		
-		
-		/**
-		 * Private Methods
-		 * 
-		 */
+
 		function createTableList(self) { // 2
 			var exceptOpts = [ 
                "buffer", "bufferCount", "csvCount", "sortLoading", "sortCache", "sortIndex", "sortOrder",
@@ -327,11 +318,6 @@ jui.defineUI("uix.xtable", [ "jquery", "util.base", "ui.modal", "uix.table" ], f
         }
 		
 
-		/**
-		 * Public Methods
-		 * 
-		 */
-		
 		this.init = function() {
 			var opts = this.options;
 
@@ -398,11 +384,24 @@ jui.defineUI("uix.xtable", [ "jquery", "util.base", "ui.modal", "uix.table" ], f
                 setColumnResizeScroll(this);
             }
 		}
-		
+
+		/**
+		 * @method select
+		 * Adds a selected class to a row at a specified index and gets an instance of the applicable row.
+		 *
+		 * @param {Integer} index
+		 * @return {RowObject} row
+		 */
 		this.select = function(index) {
 			return body.select(index);
 		}
-		
+
+		/**
+		 * @method update
+		 * Updates the list of rows or modifies the row at a specified index.
+		 *
+		 * @param {Array} rows
+		 */
 		this.update = function(dataList) {
 			rows = dataList;
 			
@@ -416,7 +415,11 @@ jui.defineUI("uix.xtable", [ "jquery", "util.base", "ui.modal", "uix.table" ], f
 				this.sort(this.options.sortIndex, this.options.sortOrder, undefined, true);
 			}
 		}
-		
+
+		/**
+		 * @method next
+		 * Changes to the next page.
+		 */
 		this.next = function() {
 			var start = (page - 1) * this.options.bufferCount,
 				end = start + this.options.bufferCount;
@@ -436,7 +439,13 @@ jui.defineUI("uix.xtable", [ "jquery", "util.base", "ui.modal", "uix.table" ], f
 				if(tmpDataList.length > 0) page++;
 			}
 		}
-		
+
+		/**
+		 * @method page
+		 * Changes to the page of at a specified index.
+		 *
+		 * @param {Integer} index
+		 */
 		this.page = function(pNo) {
 			if(this.options.buffer == "scroll") return false;
 			if(this.getPage() == pNo) return false;
@@ -447,7 +456,14 @@ jui.defineUI("uix.xtable", [ "jquery", "util.base", "ui.modal", "uix.table" ], f
 			page = (pNo < 1) ? 1 : pNo;
 			this.next();
 		}
-		
+
+		/**
+		 * @method sort
+		 * Moves a row iat a specified index to the target index.
+		 *
+		 * @param {Integer} index
+		 * @param {String} order  "asc" or "desc"
+		 */
 		this.sort = function(index, order, e, isNotLoading) { // index는 컬럼 key 또는 컬럼 name
 			if(!this.options.fields || !this.options.sort || is_resize) return;
 			
@@ -509,6 +525,12 @@ jui.defineUI("uix.xtable", [ "jquery", "util.base", "ui.modal", "uix.table" ], f
 		    }
 		}
 
+		/**
+		 * @method filter
+		 * Filters columns at a specified to locate rows that contain keywords in the cell value.
+		 *
+		 * @param {Function} callback
+		 */
         this.filter = function(callback) {
             if(typeof(callback) != "function") return;
 
@@ -528,6 +550,10 @@ jui.defineUI("uix.xtable", [ "jquery", "util.base", "ui.modal", "uix.table" ], f
             this.emit("filter", [ s_rows ]);
         }
 
+		/**
+		 * @method rollback
+		 * Returns filtered rows to the original state.
+		 */
         this.rollback = function() {
             if(o_rows != null) {
                 this.update(o_rows);
@@ -535,24 +561,42 @@ jui.defineUI("uix.xtable", [ "jquery", "util.base", "ui.modal", "uix.table" ], f
                 o_rows = null;
             }
         }
-		
+
+		/**
+		 * @method clear
+		 * Remove all row elements.
+		 */
 		this.clear = function() {
 			page = 1;
 			body.uit.removeRows();
 			body.scroll();
 		}
-		
+
+		/**
+		 * @method clear
+		 * Remove all data
+		 */
 		this.reset = function() {
 			this.clear();
 			rows = [];
 		}
-		
+
+		/**
+		 * @method resize
+		 * Resets the inner scroll and columns of a table.
+		 */
 		this.resize = function() {
 			head.resizeColumns();
 			head.resize();
 			head.emit("colresize");
 		}
-		
+
+		/**
+		 * @method height
+		 * Sets the scroll based on the height of a table.
+		 *
+		 * @param {Integer} height
+		 */
 		this.height = function(h) {
 			if(this.options.buffer != "scroll") return;
 			
@@ -561,32 +605,76 @@ jui.defineUI("uix.xtable", [ "jquery", "util.base", "ui.modal", "uix.table" ], f
 			
 			setScrollEvent(this);
 		}
-		
+
+		/**
+		 * @method size
+		 * Gets the size of all the rows of a table.
+		 *
+		 * @return {Integer} size
+		 */
 		this.size = function() { // 차후 수정 (컬럼 * 로우 개수 * 바이트)
 			return rows.length;
 		}
 
+		/**
+		 * @method count
+		 * Gets the number of trows of a table.
+		 *
+		 * @return {Integer} count
+		 */
 		this.count = function() {
 			return rows.length;
 		}
-		
+
+		/**
+		 * @method list
+		 * Gets all the rows of a table.
+		 *
+		 * @return {Array} rows
+		 */
 		this.list = function() {
 			return body.list();
 		}
-		
+
+		/**
+		 * @method listColumn
+		 * Gets all columns.
+		 *
+		 * @return {Array} columns
+		 */
 		this.listColumn = function() {
 			return head.listColumn();
 		}
-		
+
+		/**
+		 * @method listData
+		 * Gets the data of all the rows of a table.
+		 *
+		 * @return {Array} datas
+		 */
 		this.listData = function() {
 			return rows;
 		}
-		
+
+		/**
+		 * @method get
+		 * Gets the row at the specified index.
+		 *
+		 * @param {Integer} index
+		 * @return {RowObject} row
+		 */
 		this.get = function(index) {
 			if(index == null) return null;
 			return body.get(index);
 		}
-		
+
+		/**
+		 * @method getColumn
+		 * Gets the column at the specified index.
+		 *
+		 * @param {"Integer"/"String"} key index or column key
+		 * @return {ColumnObject} column
+		 */
 		this.getColumn = function(index) {
 			return head.getColumn(index);
 		}
@@ -594,46 +682,102 @@ jui.defineUI("uix.xtable", [ "jquery", "util.base", "ui.modal", "uix.table" ], f
 		this.getData = function(index) {
 			return rows[index];
 		}
-		
+
+		/**
+		 * @method showColumn
+		 * Shows the column index (or column name).
+		 *
+		 * @param {"Integer"/"String"} key index or column name
+		 */
 		this.showColumn = function(index) {
 			head.showColumn(index);
 		}
-		
+
+		/**
+		 * @method hideColumn
+		 * Hides the column index (or column name).
+		 *
+		 * @param {"Integer"/"String"} key index or column name
+		 */
 		this.hideColumn = function(index) {
 			head.hideColumn(index);
 		}
-		
+
+		/**
+		 * @method initColumns
+		 * It is possible to determine the index or name of the column to be shown in an array.
+		 *
+		 * @param {"Integer"/"String"} key index or column name
+		 */
 		this.initColumns = function(keys) {
 			head.initColumns(keys);
 			body.initColumns(keys);
 			head.emit("colresize");
 		}
-		
+
+		/**
+		 * @method showColumnMenu
+		 * Shows the Show/Hide Column menu at specified coordinates.
+		 *
+		 * @param {Integer} x
+		 */
 		this.showColumnMenu = function(x) {
 			head.showColumnMenu(x);
 		}
 
+		/**
+		 * @method hideColumnMenu
+		 * Hides the Show/Hide Column menu.
+		 */
         this.hideColumnMenu = function() {
             head.hideColumnMenu();
         }
 
+		/**
+		 * @method toggleColumnMenu
+		 * Shows or hides the Show/Hide Column menu.
+		 *
+		 * @param {Integer} x
+		 */
         this.toggleColumnMenu = function(x) {
             head.toggleColumnMenu(x);
         }
 
+		/**
+		 * @method showExpand
+		 * Shows the extended row area of a specified index.
+		 *
+		 * @param {Integer} index
+		 */
 		this.showExpand = function(index, obj) {
 			body.showExpand(index, obj);
 		}
-		
+
+		/**
+		 * @method hideExpand
+		 * Hides the extended row area of a specified index.
+		 */
 		this.hideExpand = function(index) {
 			if(index) body.hideExpand(index);
 			else body.hideExpand();
 		}
-		
+
+		/**
+		 * @method getExpand
+		 * Get a row in which the extended area is currently activated.
+		 *
+		 * @return {RowObject} row
+		 */
 		this.getExpand = function() {
 			return body.getExpand();
 		}
-		
+
+		/**
+		 * @method showLoading
+		 * Shows the loading screen for the specified delay time.
+		 *
+		 * @param {Integer} delay
+		 */
 		this.showLoading = function(delay) {
 			if(!ui_modal || is_loading) return;
 			
@@ -649,13 +793,21 @@ jui.defineUI("uix.xtable", [ "jquery", "util.base", "ui.modal", "uix.table" ], f
 			}
 		}
 
+		/**
+		 * @method hideLoading
+		 * Hides the loading screen.
+		 */
 		this.hideLoading = function() {
 			if(!ui_modal || !is_loading) return;
 			
 			ui_modal.hide();
 			is_loading = false;
 		}
-		
+
+		/**
+		 * @method setCsv
+		 * Updates a table using a CVS string.
+		 */
 		this.setCsv = function(csv) {
             var opts = this.options;
 			if(!opts.fields && !opts.csv) return;
@@ -665,7 +817,11 @@ jui.defineUI("uix.xtable", [ "jquery", "util.base", "ui.modal", "uix.table" ], f
 
 			this.update(_.csvToData(fields, csv, csvNumber));
 		}
-		
+
+		/**
+		 * @method setCsvFile
+		 * Updates a table using a CVS file.
+		 */
 		this.setCsvFile = function(file) {
 			if(!this.options.fields && !this.options.csv) return;
 			
@@ -674,7 +830,14 @@ jui.defineUI("uix.xtable", [ "jquery", "util.base", "ui.modal", "uix.table" ], f
 	            self.setCsv(csv);
 			});
 		}
-		
+
+		/**
+		 * @method getCsv
+		 * Gets the data of a table as a CSV string.
+		 *
+		 * @param {Boolean} isTree
+		 * @return {String} csv
+		 */
 		this.getCsv = function() {
 			if(!this.options.fields && !this.options.csv) return;
 			
@@ -688,13 +851,27 @@ jui.defineUI("uix.xtable", [ "jquery", "util.base", "ui.modal", "uix.table" ], f
 				names: this.options.csvNames
 			});
 		}
-		
+
+		/**
+		 * @method getCsvBase64
+		 * Gets the data of a table as a CSV string encoded as base64.
+		 *
+		 * @param {Boolean} isTree
+		 * @return {String} base64
+		 */
 		this.getCsvBase64 = function() {
 			if(!this.options.fields && !this.options.csv) return;
 			
 			return _.csvToBase64(this.getCsv());
 		}
 
+		/**
+		 * @method downloadCsv
+		 * Downloads the data of a table as a CSV file.
+		 *
+		 * @param {String} name
+		 * @param {Boolean} isTree
+		 */
         this.downloadCsv = function(name) {
             if(_.typeCheck("string", name)) {
                 name = name.split(".")[0];
@@ -708,7 +885,15 @@ jui.defineUI("uix.xtable", [ "jquery", "util.base", "ui.modal", "uix.table" ], f
             a.click();
             a.parentNode.removeChild(a);
         }
-		
+
+		/**
+		 * @method rowFunc
+		 * Ir is possible to use a function for all row data applicable to the column (or column name) of a specified column (or column name). Currently only SUM and AVG are supported.
+		 *
+		 * @param {"sum"/"svg"} funcType
+		 * @param {Integer} columnIndex
+		 * @param {Function} callback
+		 */
 		this.rowFunc = function(type, index, callback) {
 			if(!this.options.fields) return;
 			
@@ -740,11 +925,23 @@ jui.defineUI("uix.xtable", [ "jquery", "util.base", "ui.modal", "uix.table" ], f
 			
 			return null;
 		}
-		
+
+		/**
+		 * @method getPage
+		 * Gets the current page of a table.
+		 *
+		 * @return {Ingeger} page
+		 */
 		this.getPage = function() {
 			return page - 1;
 		}
-		
+
+		/**
+		 * @method activeIndex
+		 * Gets the index of a row that is activated in an extended/modified/selected state.
+		 *
+		 * @return {Integer} index
+		 */
 		this.activeIndex = function() {
 			return body.activeIndex();
 		}
@@ -752,31 +949,236 @@ jui.defineUI("uix.xtable", [ "jquery", "util.base", "ui.modal", "uix.table" ], f
 
     UI.setup = function() {
         return {
+			/**
+			 * @cfg {Array} [fields=null]
+			 * Sets the name of columns in the order of being displayed on the table screen.
+			 */
 			fields: null,
+
+			/**
+			 * @cfg {Array} [csv=null]
+			 * Sets the column key shown when converted to a CSV string.
+			 */
 			csv: null,
+
+			/**
+			 * @cfg {Array} [csvNames=null]
+			 * Sets the name of a column shown when converting to a CSV string, which must be defined in the same order as the CSV option.
+			 */
 			csvNames: null,
+
+			/**
+			 * @cfg {Array} [csvNumber=null]
+			 * Sets the column key to be changed to a number form when converted to a CSV string.
+			 */
 			csvNumber: null,
+
+			/**
+			 * @cfg {Array} [csvCount=10000]
+			 * Sets the maximum number of rows when creating a CSV string.
+			 */
 			csvCount: 10000,
+
+			/**
+			 * @cfg {Array} data
+			 * Sets the initial row list of a table.
+			 */
 			data: [],
+
+			/**
+			 * @cfg {Array} rows
+			 * Sets the initial row list of a table (@Deprecated).
+			 */
 			rows: null, // @Deprecated
+
+			/**
+			 * @cfg {Boolean/Array} [colshow=false]
+			 * Sets a column index shown when the Show/Hide Column menu is enabled.
+			 */
 			colshow: false,
+
+			/**
+			 * @cfg {Boolean} [expand=false]
+			 * Determines whether to use an extended row area.
+			 */
 			expand: false,
+
+			/**
+			 * @cfg {Boolean} [expandEvent=true]
+			 * Shows the extended area automatically when clicking on a row.
+			 */
 			expandEvent: true,
+
+			/**
+			 * @cfg {Boolean} [resize=false]
+			 * Determines whether to use the column resizing function.
+			 */
 			resize: false,
+
+			/**
+			 * @cfg {Integer} [scrollHeight=200]
+			 * Sets the reference height of a body area when using a table scroll.
+			 */
 			scrollHeight: 200,
+
+			/**
+			 * @cfg {Integer} [scrollWidth=0]
+			 * Sets the reference width of a body area when using a table scroll.
+			 */
 			scrollWidth: 0,
+
+			/**
+			 * @cfg {Integer} [width=0]
+			 * Sets the area of a table.
+			 */
 			width: 0,
+
+			/**
+			 * @cfg {String} [buffer='scroll'/'page'/'s-page']
+			 * Sets the buffer type of a table.
+			 */
 			buffer: "scroll",
+
+			/**
+			 * @cfg {Integer} [bufferCount=100]
+			 * Sets the number of rows per page.
+			 */
 			bufferCount: 100,
+
+			/**
+			 * @cfg {Boolean/Array} [sort=false]
+			 * Determines whether to use the table sort function.
+			 */
 			sort: false,
+
+			/**
+			 * @cfg {Boolean} [sortLoading=false]
+			 * Determines whether to show the loading screen when sorting a table.
+			 */
 			sortLoading: false,
+
+			/**
+			 * @cfg {Boolean} [sortCache=false]
+			 * Configures settings to ensure that the sort state can be maintained even when the table is updated.
+			 */
 			sortCache: false,
+
+			/**
+			 * @cfg {Integer} [sortIndex=null]
+			 * Determines whether to use the table sort function.
+			 */
 			sortIndex: null,
+
+			/**
+			 * @cfg {String} [sortOrder="asc"]
+			 * Determines whether to use the table sort function.
+			 */
 			sortOrder: "asc",
+
+			/**
+			 * @cfg {Boolean} [sortEvent=true]
+			 * Determines whether to use the sort function when you click on a column.
+			 */
 			sortEvent: true,
+
 			animate: false // @Deprecated
         }
     }
+
+	/**
+	 * @event select
+	 * Event that occurs when a row is selected (@Deprecated)
+	 *
+	 * @param {RowObject) row
+	 * @param {EventObject} e The event object
+	 */
+
+	/**
+	 * @event click
+	 * Event that occurs when a row is clicked
+	 *
+	 * @param {RowObject) row
+	 * @param {EventObject} e The event object
+	 */
+
+	/**
+	 * @event dblclick
+	 * Event that occurs when a row is double clicked
+	 *
+	 * @param {RowObject) row
+	 * @param {EventObject} e The event object
+	 */
+
+	/**
+	 * @event sort
+	 * Event that occurs when the table is sorted.
+	 *
+	 * @param {ColumnObject) column
+	 * @param {EventObject} e The event object
+	 */
+
+	/**
+	 * @event scroll
+	 * Event that occurs when the scroll of a table is located at the lowermost position.
+	 *
+	 * @param {EventObject} e The event object
+	 */
+
+	/**
+	 * @event rowmenu
+	 * Event that occurs when a row is right clicked.
+	 *
+	 * @param {RowObject) row
+	 * @param {EventObject} e The event object
+	 */
+
+	/**
+	 * @event colclick
+	 * Event that occurs when a column is clicked.
+	 *
+	 * @param {ColumnObject) column
+	 * @param {EventObject} e The event object
+	 */
+
+	/**
+	 * @event colshow
+	 * Event that occurs when shown column is selected.
+	 *
+	 * @param {ColumnObject) column
+	 * @param {EventObject} e The event object
+	 */
+
+	/**
+	 * @event colhide
+	 * Event that occurs when hidden column is selected.
+	 *
+	 * @param {ColumnObject) column
+	 * @param {EventObject} e The event object
+	 */
+
+	/**
+	 * @event colresize
+	 * Event that occurs when the column resizing is activated.
+	 *
+	 * @param {ColumnObject) column
+	 * @param {EventObject} e The event object
+	 */
+
+	/**
+	 * @event expand
+	 * Event that occurs when the extended row area is enabled.
+	 *
+	 * @param {RowObject) row
+	 * @param {EventObject} e The event object
+	 */
+
+	/**
+	 * @event expandend
+	 * Event that occurs when the extended row area is disabled.
+	 *
+	 * @param {RowObject) row
+	 * @param {EventObject} e The event object
+	 */
 
 	return UI;
 });
