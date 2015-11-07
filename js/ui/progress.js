2907925ed7d09aa648ee2beaf6179d37540c13f1
seogi1004 <seogi777@gmail.com>
2015-11-07 17:11:38 +0900

    프로그래스바 모듈 정리

--- js/ui/progress.js
+++ js/ui/progress.js
@@ -10,66 +10,45 @@ jui.defineUI("ui.progress", [ "jquery", "util.base" ], function($, _) {
     var UI = function() {
         var self, $root, $progress, $area, $bar;
 
-        this.init = function () {
-            self = this;
-            $root = $(this.root);
-
-            this.initElement();
+        function min() {
+            return $root.data('min') || self.options.min;
         }
 
-        this.min = function () {
-            return $root.data('min') || this.options.min;
+        function max() {
+            return $root.data('max') || self.options.max;
         }
 
-        this.max = function () {
-            return $root.data('max') || this.options.max;
+        function orient() {
+            return $root.data('orient') || self.options.orient;
         }
 
-        this.orient = function () {
-            return $root.data('orient') || this.options.orient;
+        function type() {
+            return $root.data('type') || self.options.type;
         }
 
-        this.type = function () {
-            return $root.data('type') || this.options.type;
+        function animated() {
+            return $root.data('animated') || self.options.animated;
         }
 
-        this.animated = function () {
-            return $root.data('animated') || this.options.animated;
-        }
-        this.striped = function () {
-            return $root.data('striped') || this.options.striped;
-        }
-        this.value = function () {
-            return $root.data('value') || this.options.value;
+        function striped() {
+            return $root.data('striped') || self.options.striped;
         }
 
-        this.setValue = function (value) {
-            var max = this.max();
-            var min = this.min();
-            value = (typeof value == 'undefined') ? this.value() : value;
+        function value() {
+            return $root.data('value') || self.options.value;
+        }
 
-            var percent = (value - min) / (max - min) * 100;
-            this.setBarSize(percent);
-        };
-
-        this.setBarSize = function (percent) {
-            if (this.orient() == 'vertical') {
+        function setBarSize(percent) {
+            if (orient() == 'vertical') {
                 $bar.height(percent + '%');
             } else {
                 $bar.width(percent + '%');
             }
         }
 
-        this.getValue = function (dist) {
-            var min = this.min();
-            var max = this.max();
-
-            return min + (max - min) * (parseFloat(this.getBarSize().replace('%', '')) / 100);
-        };
-
-        this.getBarSize = function () {
+        function getBarSize() {
             var percent;
-            if (this.orient() == 'vertical') {
+            if (orient() == 'vertical') {
                 percent = $bar.css('height');
             } else {
                 percent = $bar.css('width');
@@ -79,39 +58,54 @@ jui.defineUI("ui.progress", [ "jquery", "util.base" ], function($, _) {
         }
 
 
-        this.initElement = function () {
-            $progress = $("<div class='progress' />").addClass(this.orient()).addClass(this.type());
+        function initElement() {
+            $progress = $("<div class='progress' />").addClass(orient()).addClass(type());
             $area = $("<div class='area' />");
             $bar = $("<div class='bar' />");
 
-            this.setValue();
-            this.setStriped();
-            this.setAnimated();
-
+            self.setValue();
+            self.setStriped();
+            self.setAnimated();
 
             $area.html($bar);
             $progress.html($area);
             $root.html($progress);
-
         }
 
-        this.setAnimated = function (isAnimated) {
+        this.init = function () {
+            self = this;
+            $root = $(this.root);
+
+            initElement();
+        }
+
+        this.setAnimated = function(isAnimated) {
             if (typeof isAnimated == 'undefined') {
-                $bar.toggleClass('animated', this.animated());
+                $bar.toggleClass('animated', animated());
             } else {
                 $bar.toggleClass('animated', isAnimated);
             }
         }
 
-        this.setStriped = function (isStriped) {
+        this.setStriped = function(isStriped) {
             if (typeof isStriped == 'undefined') {
-                $bar.toggleClass('striped', this.striped());
+                $bar.toggleClass('striped', striped());
             } else {
                 $bar.toggleClass('striped', isStriped);
             }
         }
 
-    };
+        this.setValue = function(v) {
+            var v = (typeof v == 'undefined') ? value() : v,
+                percent = (v - min()) / (max() - min()) * 100;
+
+            setBarSize(percent);
+        }
+
+        this.getValue = function() {
+            return min() + (max() - min()) * (parseFloat(getBarSize().replace('%', '')) / 100);
+        }
+    }
 
     UI.setup = function() {
         return {
