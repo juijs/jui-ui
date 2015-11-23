b7b467c304ccb8a6fe0f2307c2669d34a48d6859
easylogic <cyberuls@gmail.com>
2015-11-24 02:24:11 +0900

    fixed floating point #386

--- dist/jui.table.js
+++ dist/jui.table.js
@@ -2472,11 +2472,13 @@ jui.define("util.math", [ "util.base" ], function(_) {
 			};
 
 			func.multi = function (a, b) {
-				return Math.round((a * pow) * (b * pow)) / pow;
+				return Math.round((a * pow) * (b * pow)) / (pow*pow);
 			};
 
 			func.div = function (a, b) {
-				return Math.round((a * pow) / (b * pow)) / pow;
+				var result = (a * pow) / (b * pow);
+				var pow2 = Math.pow(10, this.getFixed(result, 0));
+				return Math.round(result*pow2) / pow2;
 			};
 
 			func.remain = function (a, b) {
@@ -2505,12 +2507,15 @@ jui.define("util.math", [ "util.base" ], function(_) {
 
 		multi : function (a, b) {
 			var pow = Math.pow(10, this.getFixed(a, b));
-			return Math.round((a * pow) * (b * pow)) / pow;
+			return Math.round((a * pow) * (b * pow)) / (pow*pow);
 		},
 
 		div : function (a, b) {
 			var pow = Math.pow(10, this.getFixed(a, b));
-			return Math.round((a * pow) / (b * pow));
+
+			var result = (a * pow) / (b * pow);
+			var pow2 = Math.pow(10, this.getFixed(result, 0));
+			return Math.round(result*pow2) / pow2;
 		},
 
 		remain : function (a, b) {
