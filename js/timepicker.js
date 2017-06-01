d2a68089ecf97aea9c2be06f35d172fe9e5a3451
seogi1004 <seogi777@gmail.com>
2017-06-01 17:03:29 +0900

    Completed timepicker component development.

--- js/timepicker.js
+++ js/timepicker.js
@@ -9,6 +9,8 @@ jui.defineUI("ui.timepicker", [ "jquery" ], function($) {
      *
      */
     var UI = function() {
+        var $focus = null;
+
         function validNumberType(evt) {
             var theEvent = evt || window.event,
                 key = theEvent.keyCode || theEvent.which;
@@ -24,13 +26,42 @@ jui.defineUI("ui.timepicker", [ "jquery" ], function($) {
             }
         }
 
-        function updateNumberValue(elem, max) {
-            var val = parseInt($(elem).val());
+        function updateNumberValue(elem, min, max) {
+            setTimeout(function() {
+                var val = parseInt($(elem).val());
 
-            if(val > max) $(elem).val(max);
+                if(!isNaN(val)) {
+                    if(val > max) $(elem).val(max);
+                    if(val < 0) $(elem).val(min);
+                }
+            }, 100);
         }
 
-        function initElements(self) {
+        function updateUpDownValue(elem, e) {
+            if(e.which == 8 || e.which == 16 || e.which == 37 || e.which == 39) return;
+
+            var val = parseInt($(elem).val()),
+                dist = 0;
+
+            if(e.which == 38) dist = 1;
+            else if(e.which == 40) dist = -1;
+
+            var res = val + dist;
+            if(res < 0) res = 0;
+
+            setTimeout(function() {
+                $(elem).val(res < 10 ? "0" + res : res);
+            }, 100);
+        }
+
+        function updateBtnValue(elem, e) {
+            $(elem).focus();
+
+            updateUpDownValue(elem, e);
+            updateNumberValue(elem, 0, $(elem).hasClass("hours") ? 23 : 59);
+        }
+
+        function initInputElements(self) {
             var hours = self.options.hours,
                 minutes = self.options.minutes;
 
@@ -39,22 +70,88 @@ jui.defineUI("ui.timepicker", [ "jquery" ], function($) {
 
             $hours.on("keypress", validNumberType);
             $hours.on("keyup", function(e) {
-                updateNumberValue(this, 23);
+                updateUpDownValue(this, e);
+                updateNumberValue(this, 0, 23);
+                self.emit("change", [ self.getHours(), self.getMinutes() ]);
+            });
+            $hours.on("focus", function(e) {
+                $focus = $hours;
             });
 
             $minutes.on("keypress", validNumberType);
             $minutes.on("keyup", function(e) {
-                updateNumberValue(this, 59);
+                updateUpDownValue(this, e);
+                updateNumberValue(this, 0, 59);
+                self.emit("change", [ self.getHours(), self.getMinutes() ]);
+            });
+            $minutes.on("focus", function(e) {
+                $focus = $minutes;
             });
 
             $hours.val(hours < 10 ? "0" + hours : hours);
             $minutes.val(minutes < 10 ? "0" + minutes : minutes);
+
+            $focus = $hours;
+        }
+
+        function initBtnElements(self) {
+            var $icon = $(self.root).children("i"),
+                $up = $("<div></div>"),
+                $down = $("<div></div>"),
+                $hours = $(self.root).children(".hours"),
+                $minutes = $(self.root).children(".minutes");
+
+            var width = $icon.outerWidth(),
+                height = $(self.root).outerHeight(),
+                styles = {
+                    position: "absolute",
+                    width: width,
+                    height: height/2,
+                    cursor: "pointer",
+                    right: $icon.css("right")
+                };
+
+            $up.css($.extend({ top: "0px" }, styles));
+            $down.css($.extend({ top: height/2 + "px" }, styles));
+
+            $up.on("click", function(e) {
+                e.which = 38;
+                updateBtnValue($focus[0], e);
+                self.emit("change", [ self.getHours(), self.getMinutes() ]);
+            });
+            $down.on("click", function(e) {
+                e.which = 40;
+                updateBtnValue($focus[0], e);
+                self.emit("change", [ self.getHours(), self.getMinutes() ]);
+            });
+
+            $(self.root).append($up);
+            $(self.root).append($down);
         }
 
         this.init = function() {
-            initElements(this);
+            initInputElements(this);
+            initBtnElements(this);
 
             return this;
+        }
+
+        this.setHours = function(hours) {
+            var $hours = $(this.root).children(".hours").val(hours);
+            updateBtnValue($hours[0], {});
+        }
+
+        this.getHours = function() {
+            return parseInt($(this.root).children(".hours").val());
+        }
+
+        this.setMinutes = function(minutes) {
+            var $minutes = $(this.root).children(".minutes").val(minutes);
+            updateBtnValue($minutes[0], {});
+        }
+
+        this.getMinutes = function() {
+            return parseInt($(this.root).children(".minutes").val());
         }
     }
 
