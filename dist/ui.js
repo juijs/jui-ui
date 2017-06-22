5c11dc1d5cc5b4728325d9cf501431981437abd3
seogi1004 <seogi777@gmail.com>
2017-06-22 16:08:49 +0900

    Improved for use year/month/day.

--- dist/ui.js
+++ dist/ui.js
@@ -7787,6 +7787,33 @@ jui.defineUI("ui.timepicker", [ "jquery" ], function($) {
     var UI = function() {
         var $focus = null;
 
+        function getRangeMap(year, month) {
+            var lastDate = new Date(year, month, 0);
+
+            var rangeMap = {
+                min: {
+                    year: 1,
+                    month: 1,
+                    date: 1,
+                    hours: 0,
+                    minutes: 0
+                },
+                max: {
+                    year: 2100,
+                    month: 12,
+                    date: lastDate.getDate(),
+                    hours: 23,
+                    minutes: 59
+                }
+            }
+
+            return rangeMap;
+        }
+
+        function printLowNumber(value) {
+            return value < 10 ? "0" + value : value;
+        }
+
         function validNumberType(evt) {
             var theEvent = evt || window.event,
                 key = theEvent.keyCode || theEvent.which;
@@ -7807,7 +7834,7 @@ jui.defineUI("ui.timepicker", [ "jquery" ], function($) {
 
             if(!isNaN(val)) {
                 if(val > max) $(elem).val(max);
-                if(val < 0) $(elem).val(min);
+                if(val < min) $(elem).val(printLowNumber(min));
             } else {
                 $(elem).val("00");
             }
@@ -7819,38 +7846,79 @@ jui.defineUI("ui.timepicker", [ "jquery" ], function($) {
             var res = val + (dist || 0);
             if(res < 0) res = 0;
 
-            $(elem).val(res < 10 ? "0" + res : res);
+            $(elem).val(printLowNumber(res));
         }
 
-        function updateBtnValue(elem, dist, focusout) {
+        function updateBtnValue(elem, dist, min, max, focusout) {
             if(!focusout) $(elem).focus();
 
             updateUpDownValue(elem, dist);
-            updateNumberValue(elem, 0, $(elem).hasClass("hours") ? 23 : 59);
+            updateNumberValue(elem, min, max);
         }
 
         function settingKeyUpEvent(e) {
-            var dist = 0;
+            var $root = $(e.target).parent();
+
+            var dist = 0,
+                focusKey = $focus.attr("class"),
+                rangeMap = getRangeMap($root.children(".year").val(), $root.children(".month").val());
 
             if(e.which == 38) dist = 1;
             else if(e.which == 40) dist = -1;
 
             if(dist != 0) {
-                updateBtnValue(e.target, dist);
+                updateBtnValue(e.target, dist, rangeMap.min[focusKey], rangeMap.max[focusKey]);
             }
         }
 
         function initInputElements(self) {
-            var hours = self.options.hours,
+            var year = self.options.year,
+                month = self.options.month,
+                date = self.options.date,
+                hours = self.options.hours,
                 minutes = self.options.minutes;
 
-            var $hours = $(self.root).children(".hours").attr("maxlength", 2),
+            var $year = $(self.root).children(".year").attr("maxlength", 4),
+                $month = $(self.root).children(".month").attr("maxlength", 2),
+                $date = $(self.root).children(".date").attr("maxlength", 2),
+                $hours = $(self.root).children(".hours").attr("maxlength", 2),
                 $minutes = $(self.root).children(".minutes").attr("maxlength", 2);
+
+            $year.on("keypress", validNumberType);
+            $year.on("keyup", settingKeyUpEvent);
+            $year.on("focusout", function(e) {
+                updateBtnValue(this, 0, 1, 2100, true);
+            });
+            $year.on("focus", function(e) {
+                $focus = $year;
+                $focus.select();
+            });
+
+            $month.on("keypress", validNumberType);
+            $month.on("keyup", settingKeyUpEvent);
+            $month.on("focusout", function(e) {
+                updateBtnValue(this, 0, 1, 12, true);
+            });
+            $month.on("focus", function(e) {
+                $focus = $month;
+                $focus.select();
+            });
+
+            $date.on("keypress", validNumberType);
+            $date.on("keyup", settingKeyUpEvent);
+            $date.on("focusout", function(e) {
+                var rangeMap = getRangeMap($year.val(), $month.val());
+                updateBtnValue(this, 0, 1, rangeMap.max.date, true);
+            });
+            $date.on("focus", function(e) {
+                $focus = $date;
+                $focus.select();
+            });
 
             $hours.on("keypress", validNumberType);
             $hours.on("keyup", settingKeyUpEvent);
             $hours.on("focusout", function(e) {
-                updateBtnValue(this, 0, true);
+                updateBtnValue(this, 0, 0, 23, true);
             });
             $hours.on("focus", function(e) {
                 $focus = $hours;
@@ -7860,25 +7928,36 @@ jui.defineUI("ui.timepicker", [ "jquery" ], function($) {
             $minutes.on("keypress", validNumberType);
             $minutes.on("keyup", settingKeyUpEvent);
             $minutes.on("focusout", function(e) {
-                updateBtnValue(this, 0, true);
+                updateBtnValue(this, 0, 0, 59, true);
             });
             $minutes.on("focus", function(e) {
                 $focus = $minutes;
                 $focus.select();
             });
 
-            $hours.val(hours < 10 ? "0" + hours : hours);
-            $minutes.val(minutes < 10 ? "0" + minutes : minutes);
+            // 초기값 설정
+            $year.val(year);
+            $month.val(printLowNumber(month));
+            $date.val(printLowNumber(date));
+            $hours.val(printLowNumber(hours));
+            $minutes.val(printLowNumber(minutes));
 
-            $focus = $hours;
+            // 초기 선택된 넘버 박스
+            $focus = ($year.size() == 0)  ? $hours : $year;
         }
 
         function initBtnElements(self) {
             var $icon = $(self.root).children("i"),
                 $up = $("<div></div>"),
                 $down = $("<div></div>"),
+                $year = $(self.root).children(".year"),
+                $month = $(self.root).children(".month"),
+                $date = $(self.root).children(".date"),
                 $hours = $(self.root).children(".hours"),
                 $minutes = $(self.root).children(".minutes");
+
+            // 년/월/일 모드일 때, 위/아래 숫자 변경은 사용하지 않는다.
+            if($year.size() > 0) return;
 
             var width = $icon.outerWidth(),
                 height = $(self.root).outerHeight(),
@@ -7894,10 +7973,16 @@ jui.defineUI("ui.timepicker", [ "jquery" ], function($) {
             $down.css($.extend({ top: height/2 + "px" }, styles));
 
             $up.on("click", function(e) {
-                updateBtnValue($focus[0], 1);
+                var focusKey = $focus.attr("class"),
+                    rangeMap = getRangeMap($year.val(), $month.val());
+
+                updateBtnValue($focus[0], 1, rangeMap.min[focusKey], rangeMap.max[focusKey]);
             });
             $down.on("click", function(e) {
-                updateBtnValue($focus[0], -1);
+                var focusKey = $focus.attr("class"),
+                    rangeMap = getRangeMap($year.val(), $month.val());
+
+                updateBtnValue($focus[0], -1, rangeMap.min[focusKey], rangeMap.max[focusKey]);
             });
 
             $(self.root).append($up);
@@ -7911,9 +7996,38 @@ jui.defineUI("ui.timepicker", [ "jquery" ], function($) {
             return this;
         }
 
+        this.setYear = function(year) {
+            var $year = $(this.root).children(".year").val(year);
+            updateBtnValue($year[0], 0, 1, 2100);
+        }
+
+        this.getYear = function() {
+            return parseInt($(this.root).children(".year").val());
+        }
+
+        this.setMonth = function(month) {
+            var $month = $(this.root).children(".month").val(month);
+            updateBtnValue($month[0], 0, 1, 12);
+        }
+
+        this.getMonth = function() {
+            return parseInt($(this.root).children(".month").val());
+        }
+
+        this.setDate = function(date) {
+            var rangeMap = getRangeMap(this.getYear(), this.getMonth());
+            var $date = $(this.root).children(".date").val(date);
+
+            updateBtnValue($date[0], 0, 1, rangeMap.max.date);
+        }
+
+        this.getDate = function() {
+            return parseInt($(this.root).children(".date").val());
+        }
+
         this.setHours = function(hours) {
             var $hours = $(this.root).children(".hours").val(hours);
-            updateBtnValue($hours[0], 0);
+            updateBtnValue($hours[0], 0, 0, 23);
         }
 
         this.getHours = function() {
@@ -7922,7 +8036,7 @@ jui.defineUI("ui.timepicker", [ "jquery" ], function($) {
 
         this.setMinutes = function(minutes) {
             var $minutes = $(this.root).children(".minutes").val(minutes);
-            updateBtnValue($minutes[0], 0);
+            updateBtnValue($minutes[0], 0, 0, 59);
         }
 
         this.getMinutes = function() {
@@ -7934,6 +8048,9 @@ jui.defineUI("ui.timepicker", [ "jquery" ], function($) {
         var now = new Date();
 
         return {
+            year: now.getFullYear(),
+            month: now.getMonth() + 1,
+            date: now.getDate(),
             hours: now.getHours(),
             minutes: now.getMinutes()
         };
